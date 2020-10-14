ef66032a6e743192b3cdb1e9ead4e1216abe3038
Luiz <luiz@kzas.ai>
2020-10-14 15:26:10 -0300

    finishing course nextjs

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,8 +1,50 @@
+import { client } from '@/lib/prismic';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
 import {useRouter} from 'next/router';
+import { Document } from 'prismic-javascript/types/documents';
 
-export default function Product() {
+interface ProductProps {
+    product: Document;
+}
+
+export default function Product({product}: ProductProps) {
     const router = useRouter();
+
+    if (router.isFallback) {
+        return <p>Carregando...</p>
+    }
+
     return (
-    <h1>{router.query.slug}</h1>
-    )
+        <div>
+            <h1>{ PrismicDOM.RichText.asText(product.data.title) }</h1>
+
+            <img src={product.data.thumbnail.url} width="300" alt=""/>
+
+            <div dangerouslySetInnerHTML={{ __html: PrismicDOM.RichText.asHtml(product.data.description) }}></div>
+
+            <p>Price: ${product.data.price}</p>
+        </div>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: true
+    }
+}
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+    const { slug } = context.params;
+
+    const product = await client().getByUID('product', String(slug), {});
+
+    return {
+        props: {
+            product,
+        },
+        revalidate: 5
+    }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import { GetServerSideProps } from 'next';
-import {Title} from '@/styles/pages/Home';
+import Link from 'next/link';
+import { Title } from '@/styles/pages/Home';
 import SEO from '@/components/SEO';
-
-interface IProduct {
-  id: string;
-  title: string;
-}
+import { client } from '@/lib/prismic';
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
 
 interface HomeProps {
-  recommendedProducts: IProduct[];
+  recommendedProducts: Document[];
 }
 
 export default function Home({recommendedProducts}: HomeProps) {
@@ -27,8 +27,12 @@ export default function Home({recommendedProducts}: HomeProps) {
         <ul>
           {recommendedProducts.map(recommendedProduct => (
             <li key={recommendedProduct.id}>
-            {recommendedProduct.title}
-          </li>
+              <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                <a>
+                  { PrismicDOM.RichText.asText(recommendedProduct.data.title) }
+                </a>
+              </Link>
+            </li>
           ))}
         </ul>
       </section>
@@ -37,12 +41,13 @@ export default function Home({recommendedProducts}: HomeProps) {
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
-  const recommendedProducts = await response.json();
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ]);
 
   return {
     props: {
-      recommendedProducts
+      recommendedProducts: recommendedProducts.results
     }
   }
 
