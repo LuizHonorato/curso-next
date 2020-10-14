ef2ba33f430f1ff6cc91e3e441de7b17ef7de830
Luiz Honorato <lhehonorato@gmail.com>
2020-10-14 08:40:44 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import { GetServerSideProps } from 'next';
+import {Title} from '@/styles/pages/Home';
+import SEO from '@/components/SEO';
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({recommendedProducts}: HomeProps) {
+
+  return (
+    <div>
+      <SEO 
+        title="DevCommerce, your best e-commerce" 
+        image="boost.png"
+        shouldExcludeTitleSuffix={false}
+        description="Access and join the offers" />
+      <Title>Hello Luiz</Title>
+
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(recommendedProduct => (
+            <li key={recommendedProduct.id}>
+            {recommendedProduct.title}
+          </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
+
+}
